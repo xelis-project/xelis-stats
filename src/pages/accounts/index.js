1d0a6cd043d5ef2a6eda31d5cbfca4ff420ea285
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2024-08-12 12:37:01 -0400

    add table sorting in accounts page

--- src/pages/accounts/index.js
+++ src/pages/accounts/index.js
@@ -17,10 +17,21 @@ function Accounts() {
   const { t } = useLang()
   const [query, setQuery] = useQueryString({})
 
+  const [sort, _setSort] = useState(() => {
+    const key = query.sort || `total_txs`
+    const direction = query.direction || `desc`
+    return { key, direction }
+  })
+
+  const setSort = useCallback((value) => {
+    _setSort(value)
+    setQuery({ ...query, sort: value.key, direction: value.direction })
+  }, [query])
+
   const [pageState, _setPageState] = useState(() => {
     const page = parseInt(query.page) || 1
     const size = parseInt(query.size) || 20
-    return { page, size }
+    return { page, size, sort }
   })
 
   const setPageState = useCallback((value) => {
@@ -29,10 +40,11 @@ function Accounts() {
   }, [query])
 
   const offset = (pageState.page - 1) * pageState.size
+  const order = `${sort.key}::${sort.direction}`
 
   const accounts = useFetchView({
     view: `get_accounts()`,
-    params: { count: true, limit: pageState.size, offset, order: [`total_txs::desc`], }
+    params: { count: true, limit: pageState.size, offset, order: [order], }
   })
 
   const { loading, err, count, rows } = accounts
@@ -50,8 +62,26 @@ function Accounts() {
         <meta name="description" content={description} />
       </Helmet>
       <Table
-        headers={[t(`Address`), t(`Last Activity`), t(`Txs`), t(`Fees`), t(`Transfers (In / Out)`), t(`Mined Blocks`), t(`Rewards`)]}
-        list={rows} loading={loading} err={err} emptyText={t('No accounts')} colSpan={7}
+        headers={[
+          { key: `addr`, text: t(`Address`), sort: false },
+          { key: `last_activity`, text: t(`Last Activity`), sort: true },
+          { key: `total_txs`, text: t(`Txs`), sort: true },
+          { key: `total_fees`, text: t(`Fees`), sort: true },
+          { key: `in_transfers`, text: t(`Transfers In`), sort: true },
+          { key: `out_transfers`, text: t(`Transfers Out`), sort: true },
+          { key: `mined_blocks`, text: t(`Mined Blocks`), sort: true },
+          { key: `rewards`, text: t(`Rewards`), sort: true },
+        ]}
+        list={rows} loading={loading} err={err} emptyText={t('No accounts')} colSpan={8}
+        sortState={sort}
+        onSort={(header) => {
+          let direction = `desc`
+          if (sort.key === header.key) {
+            direction = sort.direction === `desc` ? `asc` : `desc`
+          }
+
+          setSort({ key: header.key, direction })
+        }}
         onItem={(item) => {
           return <React.Fragment key={item.addr}>
             <tr>
@@ -67,7 +97,10 @@ function Accounts() {
               <td>{item.total_txs.toLocaleString()}</td>
               <td>{formatXelis(item.total_fees)}</td>
               <td>
-                {item.in_transfers.toLocaleString()} | {item.out_transfers.toLocaleString()}
+                {item.in_transfers.toLocaleString()}
+              </td>
+              <td>
+                {item.out_transfers.toLocaleString()}
               </td>
               <td>{item.mined_blocks.toLocaleString()}</td>
               <td>{formatXelis(item.rewards)}</td>
